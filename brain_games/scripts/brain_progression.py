1798790fffb60e90ae5cf52fa05f25deff2a5b50
michael <catcat2005@inbox.ru>
2023-03-09 19:30:16 +0100

    add progression game

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,25 @@
+import random
+
+from brain_games.scripts.logic import play_game
+
+GAME_DESCRIPTION = 'What number is missing in the progression?'
+
+
+def generate_question():
+    length = random.randint(5, 10)
+    start = random.randint(1, 20)
+    diff = random.randint(1, 5)
+    hidden_index = random.randint(0, length - 1)
+    progression = [start + i * diff for i in range(length)]
+    correct_answer = str(progression[hidden_index])
+    progression[hidden_index] = '..'
+    question = ' '.join(str(num) for num in progression)
+    return question, correct_answer
+
+
+def main():
+    play_game(generate_question, GAME_DESCRIPTION)
+
+
+if __name__ == '__main__':
+    main()
