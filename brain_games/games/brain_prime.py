94bbe2b5c7549446245130b2244237b598f06d3f
michael <catcat2005@inbox.ru>
2023-03-09 20:51:29 +0100

    pep8

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -19,7 +19,8 @@ def generate_question():
 
 def main():
     play_game(generate_question,
-              "Answer \"yes\" if given number is prime. Otherwise answer \"no\".")
+              "Answer \"yes\" if given number is prime\
+. Otherwise answer \"no\".")
 
 
 if __name__ == '__main__':
